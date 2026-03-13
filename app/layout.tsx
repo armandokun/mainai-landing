fce7fabea26c68258e121b1374458804854564b6
armandokun <armandasvaicikauskas@protonmail.com>
2026-03-13 20:29:21 +0200

    Add favicon and remove redundant files from /public

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,14 @@ export const metadata: Metadata = {
   description: "Kur svetimi tampa pažįstamais, o paprasti dalykai sujungia.",
   manifest: "/site.webmanifest",
   metadataBase: new URL(BASE_URL),
+  icons: {
+    icon: [
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon.png", type: "image/png" },
+    ],
+    apple: [{ url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+  },
   openGraph: {
     title: "Mainai",
     description: "Kur svetimi tampa pažįstamais, o paprasti dalykai sujungia.",
